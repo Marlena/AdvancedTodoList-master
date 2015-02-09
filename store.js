9657345926f0fdf6e28c120c391b0730314fa815
Marlena Compton <marlena.compton@gmail.com>
2015-02-09 06:52:42 -0800

    finished tutorial

---
+++ store.js
@@ -0,0 +1,68 @@
+var items = []
+
+var notifyComponents = function() {
+  $(ListStore).trigger('storeHasChanged')
+}
+
+var findItemById = function(id) {
+  return items.filter(function(item) {
+    return item.id === id
+  })[0]
+},
+
+ListStore = {
+
+  getItems: function() {
+    return items
+  },
+
+  loadItems: function() {
+    var loadRequest = $.ajax({
+      type: 'GET',
+      url: "https://listalous.herokuapp.com/lists/badTurtle/"
+    });
+
+    loadRequest.done(function(dataFromServer){
+      items = dataFromServer.items;
+      notifyComponents();
+    })
+  },
+  addItem: function(itemDescription) {
+    var creationRequest = $.ajax({
+      type: 'POST',
+      url: 'http://listalous.herokuapp.com/lists/badTurtle/items',
+      data: {description: itemDescription, completed: false }
+      })
+
+    creationRequest.done(function(itemDataFromServer){
+      items.push(itemDataFromServer)
+      notifyComponents()
+    })
+  },
+  toggleCompleteness: function(itemId) {
+    var item = findItemById(itemId)
+    var currentCompletedValue = item.completed
+
+    var updateRequest = $.ajax({
+      type: 'PUT',
+      url: 'https://listalous.herokuapp.com/lists/badTurtle/items/' + itemId,
+      data: { completed: !currentCompletedValue }
+    })
+
+    updateRequest.done(function(itemData){
+      item.completed = itemData.completed
+      notifyComponents()
+    })
+  },
+  deleteItem: function(itemId) {
+
+    var updateRequest = $.ajax({
+      type: 'DELETE',
+      url: 'https://listalous.herokuapp.com/lists/badTurtle/items/' + itemId,
+    });
+
+    updateRequest.done(function(itemData){
+      notifyComponents()
+    })
+  }
+}
